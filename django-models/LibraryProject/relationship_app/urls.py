9c78fb5c81484c032aa7856aa00ecd7ad9eb1723
Lorrah <lorrahngobeni.ln@gmail.com>
2025-07-27 22:39:49 +0200

    User profile

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -36,8 +36,8 @@ urlpatterns = [
     path('admin-page/', views.admin_view, name='admin_view'),
     path('librarian-page/', views.librarian_view, name='librarian_view'),
     path('member-page/', views.member_view, name='member_view'),
-    path('books/add/', views.add_book, name='add_book'),
-    path('books/edit/<int:pk>/', views.edit_book, name='edit_book'),
-    path('books/delete/<int:pk>/', views.delete_book, name='delete_book'),
+    path('books/add_book/', views.add_book, name='add_book'),
+    path('books/edit_book/<int:book_id>/', views.edit_book, name='edit_book'),
+    path('books/delete/<int:book_id>/', views.delete_book, name='delete_book'),
     
 ]
