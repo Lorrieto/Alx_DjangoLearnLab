4514d8e9e78fe19fcf6fc75a45c48f89d29b3919
Lorrah <lorrahngobeni.ln@gmail.com>
2025-07-27 02:12:14 +0200

    Login

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -28,7 +28,8 @@ urlpatterns = [
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-     path('register/', register, name='register'),
+    path('register/', register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logged_out.html'), name='logout'),
+    
 ]
