41dd5d4eeef6d03d29024608839f21ff03dbb2eb
Lorry <lorrahngobeni.ln@gmail.com>
2025-08-08 23:27:58 +0200

    Additional folders

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,4 +1,4 @@
-# api/views.py
+
 from rest_framework import viewsets, permissions
 from .models import Book
 from .serializers import BookSerializer
@@ -6,4 +6,4 @@ from .serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticated]  # only authenticated users allowed
+    permission_classes = [permissions.IsAuthenticated]  
