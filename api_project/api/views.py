19486baff82579a9028dc49ab89c0ec2c9715aa4
Lorry <lorrahngobeni.ln@gmail.com>
2025-08-08 16:56:05 +0200

    Booklist

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()       # retrieves all Book records
+    serializer_class = BookSerializer   # uses our serializer
 
 # Create your views here.
