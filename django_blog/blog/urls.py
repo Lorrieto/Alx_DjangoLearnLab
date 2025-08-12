1c6fba03318ca6e13f68520d387194e4395156c7
Lorry <lorrahngobeni.ln@gmail.com>
2025-08-12 16:32:54 +0200

    Fixed the url

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -9,8 +9,8 @@ from .views import (
 
 urlpatterns = [
     path('posts/', PostListView.as_view(), name='post-list'),
-    path('posts/new/', PostCreateView.as_view(), name='post-create'),
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
-    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('posts/new/', PostCreateView.as_view(), name='post-new'),  # create
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),  # read
+    path('posts/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),  # update
+    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),  # delete
 ]
