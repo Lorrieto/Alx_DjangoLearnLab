ff9342eeb4e31027163ca492e7b8d965ab746129
Lorry <lorrahngobeni.ln@gmail.com>
2025-08-14 13:34:18 +0200

    fixed api views

--- advanced_api_project/api/views.py
+++ advanced_api_project/api/views.py
@@ -1,30 +1,32 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
 from .models import Book
 from .serializers import BookSerializer
 
-# LIST VIEW (Read-Only for all)
+# LIST VIEW
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.AllowAny]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['title', 'author']
+    ordering_fields = ['published_date', 'title']
 
-# DETAIL VIEW (Read-Only for all)
+# DETAIL VIEW
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.AllowAny]
 
-# CREATE VIEW (Authenticated Only)
+# CREATE VIEW
 class BookCreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticated]
 
-    # Example: Automatically set the user who created the book
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
 
-# UPDATE VIEW (Authenticated Only)
+# UPDATE VIEW
 class BookUpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
@@ -33,7 +35,7 @@ class BookUpdateView(generics.UpdateAPIView):
     def perform_update(self, serializer):
         serializer.save(updated_by=self.request.user)
 
-# DELETE VIEW (Authenticated Only)
+# DELETE VIEW
 class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
