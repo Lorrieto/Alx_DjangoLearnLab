0576a198230eac8cd55d8c6e4f32e6295ab027d7
Lorry <lorrahngobeni.ln@gmail.com>
2025-08-18 13:10:12 +0200

    updated the create account in serializer.py script

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,37 +1,21 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
-from .models import User
 
-
-class UserSerializer(serializers.ModelSerializer):
-    followers_count = serializers.IntegerField(source='followers.count', read_only=True)
-    following_count = serializers.IntegerField(source='following.count', read_only=True)
-
-
-    class Meta:
-        model = User
-        fields = [
-        'id', 'username', 'email', 'first_name', 'last_name',
-        'bio', 'profile_picture', 'followers_count', 'following_count',
-        'date_joined'
-        ]
-        read_only_fields = ['id', 'followers_count', 'following_count', 'date_joined']
+User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=8)
-
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
 
-
     def create(self, validated_data):
-        user = User.objects.create_user(
-        username=validated_data['username'],
-        email=validated_data.get('email', ''),
-        password=validated_data['password']
+        user = get_user_model().objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            password=validated_data['password']
         )
         Token.objects.create(user=user)
         return user
