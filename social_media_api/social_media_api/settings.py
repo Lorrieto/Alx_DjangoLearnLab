1f2724a2ee8d81d67732b009f26bace46992cf0c
Lorry <lorrahngobeni.ln@gmail.com>
2025-08-22 18:19:36 +0200

    settings.py

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -145,3 +145,11 @@ REST_FRAMEWORK = {
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_SSL_REDIRECT = True  
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+MEDIA_ROOT = BASE_DIR / 'mediafiles'
