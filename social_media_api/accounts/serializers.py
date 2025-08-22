72c1d6bc994934cddf5987d974aaef88bf0121c5
Lorry <lorrahngobeni.ln@gmail.com>
2025-08-22 15:56:57 +0200

    updated settings

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,34 +1,22 @@
-from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from rest_framework.authtoken.models import Token
+from .models import Post, Comment
+from accounts.serializers import UserSerializer
 
-User = get_user_model()
 
-class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, min_length=8)
+class CommentSerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
+
 
     class Meta:
-        model = User
-        fields = ['username', 'email', 'password']
-
-    def create(self, validated_data):
-        user = get_user_model().objects.create_user(
-            username=validated_data['username'],
-            email=validated_data.get('email', ''),
-            password=validated_data['password']
-        )
-        Token.objects.create(user=user)
-        return user
+        model = Comment
+        fields = ['id', 'author', 'content', 'created_at', 'updated_at']
 
 
-class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField(write_only=True)
+class PostSerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
+    comments = CommentSerializer(many=True, read_only=True)
 
 
-    def validate(self, attrs):
-        user = authenticate(username=attrs['username'], password=attrs['password'])
-        if not user:
-            raise serializers.ValidationError('Invalid credentials.')
-        attrs['user'] = user
-        return attrs
+    class Meta:
+        model = Post
+        fields = ['id', 'author', 'title', 'content', 'created_at', 'updated_at', 'comments']
