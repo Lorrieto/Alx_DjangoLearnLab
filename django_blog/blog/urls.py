ca2cb954917170e91ff79f5d66e3c2c97019cfd2
Lorry <lorrahngobeni.ln@gmail.com>
2025-08-24 22:06:51 +0200

    url link

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -14,5 +14,6 @@ urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    #"post/<int:pk>/update/"
 ]
 
