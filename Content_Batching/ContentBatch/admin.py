2fc59956fb540dc71cfe9de5a85c6eeeb9b3c6c4
Lorry <lorrahngobeni.ln@gmail.com>
2025-09-10 23:15:44 +0200

    Created content items for batch admin

--- Content_Batching/ContentBatch/admin.py
+++ Content_Batching/ContentBatch/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
-from .models import Batch
+from .models import Batch, ContentItem
+
+class ContentItemInline(admin.TabularInline):
+    model = ContentItem
+    extra = 1
+    fields = ("title", "platform", "scheduled_date", "media")
 
 @admin.register(Batch)
 class BatchAdmin(admin.ModelAdmin):
-    list_display = ("name", "created_by", "created_at")
+    list_display = ("name", "created_at")
     search_fields = ("name", "description")
+    inlines = [ContentItemInline]
+
+@admin.register(ContentItem)
+class ContentItemAdmin(admin.ModelAdmin):
+    list_display = ("title", "platform", "batch", "scheduled_date", "created_at")
+    list_filter = ("platform", "scheduled_date")
+    search_fields = ("title", "body", "notes")
