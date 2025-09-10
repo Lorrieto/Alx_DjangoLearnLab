09af1d345ee17838597f64cbf882d6f0e7f5b4e5
Lorry <lorrahngobeni.ln@gmail.com>
2025-09-10 22:56:47 +0200

    Defined the batch model

--- Content_Batching/ContentBatch/models.py
+++ Content_Batching/ContentBatch/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Batch(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="batches")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
